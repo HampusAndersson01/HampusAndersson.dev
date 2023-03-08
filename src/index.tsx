b4d8e6929de9c10d1ed46232b87e38f40917017d
Hampus Andersson <hampe.01@hotmail.com>
2023-03-08 12:36:34 +0100

    added basename

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import "./index.css";
 import { DarkModeContext, DarkModeProvider } from "./context/DarkMode";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage></HomePage>,
-  },
-  {
-    path: "about",
-    element: <AboutPage></AboutPage>,
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <HomePage></HomePage>,
+    },
+    {
+      path: "about",
+      element: <AboutPage></AboutPage>,
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <DarkModeProvider>
